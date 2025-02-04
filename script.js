93e2a8433306ae7fba9982d197d7e0f2db80a928
Trushar Patel <23dit054@charusat.edu.in>
2025-02-04 18:42:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-document.querySelector(".cta-button").addEventListener("click", function () {
-  alert("Thank you for showing interest!");
-});
-
-window.addEventListener("resize", function () {
-  if (window.innerWidth < 768) {
-    document.querySelector(".cta-button").style.fontSize = "14px";
-  } else {
-    document.querySelector(".cta-button").style.fontSize = "18px";
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
   }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
 });
